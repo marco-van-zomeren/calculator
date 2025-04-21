f5ac27bd0a14d7f62d877e38a7a54b44bf15f73a
marco-van-zomeren <m.g.p.vanzomeren@gmail.com>
2025-04-21 22:57:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 const planetCodes = {
   Sun: '10',
