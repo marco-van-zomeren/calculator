44b2418bd42cfea19c787443275fbed4f339afc8
marco-van-zomeren <m.g.p.vanzomeren@gmail.com>
2025-04-22 20:02:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://designonacid.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
